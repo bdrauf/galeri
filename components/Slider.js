ed3359198a3fb67d47b742c30774a3cb10070d87
kelindanid <kelindan.id@gmail.com>
2023-10-19 17:41:12 +0700

    foto

--- components/Slider.js
+++ components/Slider.js
@@ -9,11 +9,7 @@ const Slider = () => {
     <div>
       <Carousel>
         <Carousel.Item>
-          <img className="d-block w-full" src="/peta.jpg" />
-          <Carousel.Caption style={captionStyle}>
-            <h3>Peta Kelana</h3>
-            <p>Kesana kemari berbagi</p>
-          </Carousel.Caption>
+          <img className="d-block w-full" src="/petax.jpg" />
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-full" src="sudut1q.jpg" />
@@ -23,7 +19,21 @@ const Slider = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-full" src="sudut2q.jpg" />
+          <img className="d-block w-full" src="sudut2x.jpg" />
+          <Carousel.Caption>
+            <h3>Forum Sudut Pandang</h3>
+            <p>Palu, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sudut3.jpg" />
+          <Carousel.Caption>
+            <h3>Forum Sudut Pandang</h3>
+            <p>Palu, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sudut4.png" />
           <Carousel.Caption>
             <h3>Forum Sudut Pandang</h3>
             <p>Palu, Indonesia</p>
@@ -38,6 +48,20 @@ const Slider = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-full" src="siku2q.jpg" />
+          <Carousel.Caption>
+            <h3>Siku Ruang Terpadu</h3>
+            <p>Makassar, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="siku3.jpg" />
+          <Carousel.Caption>
+            <h3>Siku Ruang Terpadu</h3>
+            <p>Makassar, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="siku4.jpg" />
           <Carousel.Caption>
             <h3>Siku Ruang Terpadu</h3>
             <p>Makassar, Indonesia</p>
@@ -69,6 +93,111 @@ const Slider = () => {
           <Carousel.Caption>
             <h3>Hysteria</h3>
             <p>Semarang, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="pasir1.jpg" />
+          <Carousel.Caption>
+            <h3>Pasir Putih</h3>
+            <p>Lombok, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="pasir2.jpg" />
+          <Carousel.Caption>
+            <h3>Pasir Putih</h3>
+            <p>Lombok, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="kahe1.jpg" />
+          <Carousel.Caption>
+            <h3>Komunitas Kahe</h3>
+            <p>Flores, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="kahe2.jpg" />
+          <Carousel.Caption>
+            <h3>Komunitas Kahe</h3>
+            <p>Flores, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sikel1.jpg" />
+          <Carousel.Caption>
+            <h3>Sikukeluang</h3>
+            <p>Pekanbaru, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sikel2.jpg" />
+          <Carousel.Caption>
+            <h3>Sikukeluang</h3>
+            <p>Pekanbaru, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gubuk1.jpg" />
+          <Carousel.Caption>
+            <h3>Gubuak Kopi</h3>
+            <p>Solok, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gubuk2.jpg" />
+          <Carousel.Caption>
+            <h3>Gubuak Kopi</h3>
+            <p>Solok, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gubuk3.jpg" />
+          <Carousel.Caption>
+            <h3>Gubuak Kopi</h3>
+            <p>Solok, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sinau1.jpg" />
+          <Carousel.Caption>
+            <h3>Sinau Art</h3>
+            <p>Cirebon, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="sinau2.jpg" />
+          <Carousel.Caption>
+            <h3>Sinau Art</h3>
+            <p>Cirebon, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gor1.jpg" />
+          <Carousel.Caption>
+            <h3>Gelanggang Olah Rasa</h3>
+            <p>Bandung, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gor2.jpg" />
+          <Carousel.Caption>
+            <h3>Gelanggang Olah Rasa</h3>
+            <p>Bandung, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gor3.jpg" />
+          <Carousel.Caption>
+            <h3>Gelanggang Olah Rasa</h3>
+            <p>Bandung, Indonesia</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-full" src="gor4.jpg" />
+          <Carousel.Caption>
+            <h3>Gelanggang Olah Rasa</h3>
+            <p>Bandung, Indonesia</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
