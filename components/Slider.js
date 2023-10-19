f515535893e59faaeaf9dc967ce943494cb5138b
kelindanid <kelindan.id@gmail.com>
2023-10-19 13:00:30 +0700

    siku

--- components/Slider.js
+++ components/Slider.js
@@ -30,7 +30,7 @@ const Slider = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-full" src="siku1.png" />
+          <img className="d-block w-full" src="siku1.jpg" />
           <Carousel.Caption>
             <h3>Lumbung Kelana</h3>
             <p>Kesana kemari berbagi</p>
